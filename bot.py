b68f03c22bfbea8728482ba935be3f77924caee5
Steven Collins <StevenCollins@users.noreply.github.com>
2021-11-08 21:47:48 -0500

    Cuteness can now be directed

--- bot.py
+++ bot.py
@@ -74,7 +74,7 @@ async def on_message(message):
             await message.channel.send(getTimeString())
         if "version" in message.content.lower() or "1." in message.content.lower():
             await message.channel.send(getVersionString())
-    elif any(word in message.content.lower() for word in ["owo", "uwu"]):
+    if any(word in message.content.lower() for word in ["owo", "uwu"]):
         await message.channel.send("*nuzzles you*")
 
     await bot.process_commands(message)
